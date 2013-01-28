Ghilt/Enduro4

commit 6a7bde4419a10d13b89816722fc50f905e3c79f5

Fix formatColumn(s) to type Object and their uses. Andrée Karl

Enduro/src/sort/Time.java
@@ -11,6 +11,7 @@ public class Time {
 		return new Time((System.currentTimeMillis() / 1000) % 86400);
 	}
 
+	private static final String SEPARATOR = ".";
 	public static final String NULL_TIME = "--.--.--";
 	private long seconds;
 
@@ -44,7 +45,7 @@ public class Time {
 		if (secString.length() < 2) {
 			secString = "0" + secString;
 		}
-		return ((hours >= 10) ? hours : "0" + hours) + ";" + minString + ";" + secString;
+		return ((hours >= 10) ? hours : "0" + hours) + SEPARATOR + minString + SEPARATOR + secString;
 	}
 
 	@Override

Enduro/src/sort/Competitor.java
@@ -79,10 +79,10 @@ public class Competitor {
 	}
 	
 	public String toString() {
-		return Sorter.formatColumns(String.valueOf(index), 
+		return Sorter.formatColumns(index, 
 				totalTimeToString(), 
-				(startTimes.isEmpty() ? NO_START : startTimes.get(0)).toString(),
-				(finishTimes.isEmpty() ? NO_END : finishTimes.get(0)).toString());
+				(startTimes.isEmpty() ? NO_START : startTimes.get(0)),
+				(finishTimes.isEmpty() ? NO_END : finishTimes.get(0)));
 	}
 
 }

Enduro/src/sort/Sorter.java
@@ -54,14 +54,14 @@ public class Sorter {
 	 * @param s String
 	 * @return String as column
 	 */
-	public static String formatColumn(String s) {
-		return s + "; ";
+	public static String formatColumn(Object o) {
+		return o + "; ";
 	}
 	
-	public static String formatColumns(String... strings) {
+	public static String formatColumns(Object... objs) {
 		StringBuilder sb = new StringBuilder();
-		for (String s : strings) {
-			sb.append(formatColumn(s));
+		for (Object o : objs) {
+			sb.append(formatColumn(o));
 		}
 		
 		return sb.toString();

Enduro/src/test/TestCompetitor.java
@@ -11,6 +11,7 @@ public class TestCompetitor {
 	
 	private Competitor c;
 	
+	
 	@Before
 	public void setup() {
 		c = new Competitor(1);
@@ -20,19 +21,19 @@ public class TestCompetitor {
 	public void testGoodTimes() {
 		c.addStartTime(new Time(10));
 		c.addFinishTime(new Time(45));
-		assertEquals(Sorter.formatColumns("1", "00;00;35", "00;00;10", "00;00;45"), c.toString());
+		assertEquals(Sorter.formatColumns(1, new Time(35), new Time(10), new Time(45)), c.toString());
 	}
 	
 	@Test
 	public void testBadStart() {
 		c.addFinishTime(new Time(45));
-		assertEquals(Sorter.formatColumns("1", Time.NULL_TIME, Competitor.NO_START, "00;00;45"), c.toString());
+		assertEquals(Sorter.formatColumns(1, Time.NULL_TIME, Competitor.NO_START, new Time(45)), c.toString());
 	}
 	
 	@Test
 	public void testBadEnd() {
 		c.addStartTime(new Time(10));		
-		assertEquals(Sorter.formatColumns("1", Time.NULL_TIME, "00;00;10", Competitor.NO_END), c.toString());
+		assertEquals(Sorter.formatColumns(1, Time.NULL_TIME, new Time(10), Competitor.NO_END), c.toString());
 	}
 
 }

commit 8b5105d4f4801a6b7f31fa835fa06cbfd39aa462

Fixed regression from commit 6a7bde4419a10d13b89816722fc50f905e3c79f5 Changed Time separator to ".". Andrée Karl

Enduro/src/sort/Time.java
@@ -83,10 +83,11 @@ public class Time {
 	}
 
 	private long convertToTime(String stringTime) {
+		
 		long i;
 		try {
-			String[] stamps = stringTime.split(";");
-			// System.out.println(stamps.length+" "+stamps[0]+" "+stamps[1]+" "+stamps[2]);
+			String[] stamps = stringTime.split("\\" + SEPARATOR);
+//			 System.out.println(stamps.length+" "+stamps[0]+" "+stamps[1]+" "+stamps[2]);
 			Long hours = Long.parseLong(stamps[0]);
 			Long minutes = Long.parseLong(stamps[1]);
 			Long seconds = Long.parseLong(stamps[2]);

Enduro/src/test/TestTime.java
@@ -33,12 +33,12 @@ public class TestTime {
 	
 	@Test
 	public void testConvertFromStringSimple() {
-		assertEquals(new Time(new String("01;00;00")), new Time(3600));
+		assertEquals(new Time(new String("01.00.00")), new Time(3600));
 	}
 	
 	@Test
 	public void testConvertFromStringComplicated() {
-		assertEquals(new Time("05;23;01"), new Time(19381));
+		assertEquals(new Time("05.23.01"), new Time(19381));
 	}
 	
 	@Test
@@ -48,12 +48,12 @@ public class TestTime {
 	
 	@Test
 	public void testPrintSimpleTime() {
-		assertEquals(new Time(3600).toString(), "01;00;00");
+		assertEquals(new Time(3600).toString(), "01.00.00");
 	}
 	
 	@Test
 	public void testPrintComplicatedTime() {
-		assertEquals(new Time(19381).toString(), "05;23;01");
+		assertEquals(new Time(19381).toString(), "05.23.01");
 	}
 
 }