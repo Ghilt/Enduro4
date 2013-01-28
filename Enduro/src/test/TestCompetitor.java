39a2e3debefed2b694bc50db601d4ffcaff5ce5f
Philip Stahl <ada10pst@alfa-15.student.lth.se>
2013-01-28 11:08:28 +0100

    Added more Competitor tests. Andrée Karl

--- Enduro/src/test/TestCompetitor.java
+++ Enduro/src/test/TestCompetitor.java
@@ -20,8 +20,19 @@ public class TestCompetitor {
 	public void testGoodTimes() {
 		c.addStartTime(new Time(10));
 		c.addFinishTime(new Time(45));
-		
 		assertEquals(Sorter.formatColumns("1", "00;00;35", "00;00;10", "00;00;45"), c.toString());
+	}
+	
+	@Test
+	public void testBadStart() {
+		c.addFinishTime(new Time(45));
+		assertEquals(Sorter.formatColumns("1", Time.NULL_TIME, Competitor.NO_START, "00;00;45"), c.toString());
+	}
+	
+	@Test
+	public void testBadEnd() {
+		c.addStartTime(new Time(10));		
+		assertEquals(Sorter.formatColumns("1", Time.NULL_TIME, "00;00;10", Competitor.NO_END), c.toString());
 	}
 
 }
