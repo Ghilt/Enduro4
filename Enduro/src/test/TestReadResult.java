66cc994fc2421b94109a58f4baf43972afa8df07
dat11hli <dat11hli@alfa-11.student.lth.se>
2013-01-28 14:17:44 +0100

    Add starttime and finishtime into hashmap! Jacob och Victor

--- Enduro/src/test/TestReadResult.java
+++ Enduro/src/test/TestReadResult.java
@@ -4,29 +4,42 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import result.ReadResult;
 
 public class TestReadResult {
 	private ReadResult result;
+	
+	@Before
+	public void initialize(){
+		result = new ReadResult(new File("register_simple.txt"), new File("start_simple.txt"), new File("end_simple"));
+	}
 
 
 	@Test
 	public void testAddRegToResult() {
-		result = new ReadResult(new File("register_simple.txt"), new File("start_simple.txt"), new File("end_simple.txt"));
 		assertFalse(result.openResultFile().size()== 0);
 	}
-	/**
+	
 	@Test
 	public void testAddStartToResult(){
-		result = new ReadResult(new File("register_simple.txt"), new File("start_simple.txt"), new File("end_simple.txt"));
-		assertTrue(result.openResultFile().get(0).getStartTimes().get(0)!=null);
+		assertTrue(result.openResultFile().get(1).getStartTimes().get(0)!=null);
 	}
 	@Test
 	public void testAddEndToResult(){
-		result = new ReadResult(new File("register_simple.txt"), new File("start_simple.txt"), new File("end_simple.txt"));
-		assertTrue(result.openResultFile().get(0).getFinishTimes().get(0)!=null);
+		assertTrue(result.openResultFile().get(1).getFinishTimes().get(0)!=null);
 	}
-**/
+	
+	@Test
+	public void testStartTimeAdded(){
+		assertEquals(result.openResultFile().get(1).getStartTimes().get(0).toString(), "00.00.00");
+	}
+	
+	@Test
+	public void testFinishTimeAdded(){
+		assertEquals(result.openResultFile().get(1).getFinishTimes().get(0).toString(), "01.40.40");
+	}
+
 }	
