1018688902582ee8814abbc7f2db8a6b619a99a4
Rickard Johansson <dat11rjo@alfa-15.student.lth.se>
2013-02-04 16:37:40 +0100

    Test9, ignore. Rickard & Andrée

--- Enduro/src/acceptanstest9/Test9.java
+++ Enduro/src/acceptanstest9/Test9.java
@@ -1,10 +1,16 @@
 package acceptanstest9;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import members.*;
@@ -12,33 +18,41 @@ import members.*;
 import result.CvsReader;
 import result.Parser;
 import result.ParserException;
+import sort.CompetitorPrinter;
+import sort.LapCompetitorPrinter;
 
 public class Test9 {
-	private CvsReader reader;
 	private Parser parser;
-	
-
 	
 	@Before
 	public void initialize(){
 		parser = new Parser();
-			
-}
+	}
 	
-	@Test
+	@Ignore
 	public void testResult() throws FileNotFoundException, ParserException{
-		reader= new CvsReader("src/acceptanstest9/namnfil.txt");
-		Map <Integer, Competitor> c = new HashMap<Integer, Competitor>();
+		Map<Integer, Competitor> competitors;
 		
-		parser.parse(reader.readAll(), c);
+		competitors = parser.parse(new CvsReader("src/acceptanstest9/starttider.txt").readAll());
+		parser.parse(new CvsReader("src/acceptanstest9/maltider.txt").readAll(), competitors);
+		parser.parse(new CvsReader("src/acceptanstest9/namnfil.txt").readAll(), competitors);
 		
-		reader = new CvsReader("src/acceptanstest9/starttider.txt");
-		parser.parse(reader.readAll(), c);
-		
-		reader = new CvsReader("src/acceptanstest9/maltider.txt");
-		parser.parse(reader.readAll(), c);
-		
-		
+		LapCompetitorPrinter printer = new LapCompetitorPrinter();
+		printer.printResults(new ArrayList<Competitor>(competitors.values()), "src/test/tmp/accept9_result.txt");
+		testResultFiles();
+	}
+	
+	private void testResultFiles() throws FileNotFoundException {
+		File file1 = new File("src/acceptanstest9/resultat.txt");
+		File file2 = new File("src/test/tmp/accept9_result.txt");
+		Scanner scan1 = new Scanner(file1);
+		Scanner scan2 = new Scanner(file2);
+		String line1, line2;
+		while (scan1.hasNext() && scan2.hasNext()) {
+			line1 = scan1.nextLine();
+			line2 = scan2.nextLine();
+			assertEquals("Wrong result.", line1, line2);
+		}
 	}
 	
 	
