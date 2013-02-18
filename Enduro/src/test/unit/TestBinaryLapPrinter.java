0396f91a71be7ac4077417c6a6d9efa848a8167c
Henrik <dat11hli@student.lu.se>
2013-02-18 10:43:30 +0100

    Refactoring to TestBinaryPrinter. Henrik Oskar

--- Enduro/src/test/unit/TestBinaryLapPrinter.java
+++ Enduro/src/test/unit/TestBinaryLapPrinter.java
@@ -19,7 +19,7 @@ public class TestBinaryLapPrinter {
 		cp = new BinaryLapPrinter();
 	}
 
-	@Ignore
+	@Test
 	public void testGoodTimes() {
 		String start1 = "00.00.15";
 		String finish1 = "00.45.00";
@@ -29,31 +29,33 @@ public class TestBinaryLapPrinter {
 
 		Time etapp1 = Time.parse(start1).difference(Time.parse(finish1));
 
-		assertEquals(
-				Formater.formatColumns(1, c.getName(), etapp1, 1, etapp1,
-						Time.parse(start1), Time.parse(finish1)), cp.row(c));
+		String formattedRow = Formater.formatColumns(1, c.getName(), etapp1,
+				1, etapp1, Time.parse(start1), Time.parse(finish1));
+
+		assertEquals(formattedRow, cp.row(c));
 	}
 
-	@Ignore
+	@Test
 	public void testTwoGoodTimes() {
-		String start1 = "00.00.15";
-		String finish1 = "00.45.00";
-		String start2 = "01.00.00";
-		String finish2 = "01.40.00";
+		Time start1 = Time.parse("00.00.15");
+		Time finish1 = Time.parse("00.45.00");
+		Time start2 = Time.parse("01.00.00");
+		Time finish2 = Time.parse("01.40.00");
 
-		c.addStartTime(Time.parse(start1));
-		c.addStartTime(Time.parse(start2));
-		c.addFinishTime(Time.parse(finish1));
-		c.addFinishTime(Time.parse(finish2));
+		c.addStartTime(start1);
+		c.addStartTime(start2);
+		c.addFinishTime(finish1);
+		c.addFinishTime(finish2);
 
-		Time etapp1 = Time.parse(start1).difference(Time.parse(finish1));
-		Time etapp2 = Time.parse(start2).difference(Time.parse(finish2));
-		Time total = Time.parse(start1).difference(Time.parse(finish1))
-				.add(etapp2);
+		Time etapp1 = start1.difference(finish1);
+		Time etapp2 = start2.difference(finish2);
+		Time total = etapp1.clone().add(etapp2);
 
-		assertEquals(Formater.formatColumns(1, c.getName(), total, 2, etapp1,
-				etapp2, Time.parse(start1), Time.parse(finish1),
-				Time.parse(start2), Time.parse(finish2)), cp.row(c));
+		String formattedRow = Formater.formatColumns(1, c.getName(), total, 2,
+				etapp1, etapp2, start1.clone(), finish1.clone(),
+				start2.clone(), finish2.clone());
+
+		assertEquals(formattedRow, cp.row(c));
 	}
 
 }
